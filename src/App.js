c11aca8d8ebc605dccaad54ac421c39a25e4eb44
Friedrich <friedrich.ten.hagen@googlemail.com>
2023-04-10 08:56:04 +0200

    Add randomly generated keys to all task objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import { Overview } from './Overview.js'
+import { useState } from 'react';
 
 function App() {
+  const [taskList, setTaskList] = useState([{task: "go shopping", id: 123}, {task: "go gym", id: 4555}]);
+
+  //const tasks = taskList.map(task => <li key={task}>{task}</li>)
+
+
   return (
     <div className="App">
-      <Overview/>
+      <Overview taskList={taskList} setTaskList={setTaskList}/>
+
+
+
+      {/* <ul>
+        {tasks}
+      </ul> */}
     </div>
+    
+
   );
 }
 
--- src/Overview.js
+++ src/Overview.js
@@ -1,12 +1,27 @@
-export function Overview(){
-    
+export function Overview({taskList, setTaskList}){
+    // create a serial number to be used as a key in the list items
+    let todoId = 0  
+
     function handleFormSubmit(e){
+        // prevent the form from sending http request
         e.preventDefault()
+        // get data from form
         const form = e.target
         const formData = new FormData(form)
-        console.log(formData)
         const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson.todo);
+        //console.log(formJson)
+
+
+
+        let newTodoObject = {
+            task: formJson.todo, 
+            id: crypto.randomUUID(),
+        }
+
+        // save the todo into state
+        setTaskList([...taskList, newTodoObject])
+
+        console.log(taskList)
     }
     
     
@@ -14,9 +29,9 @@ export function Overview(){
         <div>
             <form onSubmit={handleFormSubmit} >
                 <input defaultValue="Some initial value" name="todo"/>
+                <input type="checkbox" name="checkbox"></input>
                 <button type="submit">Submit todo</button>
             </form>
-
         </div>
     )
 }
